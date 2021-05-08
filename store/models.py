54e2f46d8098f371e4c7f0e12bb7e125bf8d276d
Daniel Santos <dados.dan.santos@gmail.com>
2021-05-08 17:24:18 -0300

    fix: pep8 adequation

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Category(models.Model):
     name = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, unique=True)
@@ -10,11 +11,13 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-        
+
 
 class Product(models.Model):
-    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='product_creator')
+    category = models.ForeignKey(
+        Category, related_name='product', on_delete=models.CASCADE)
+    created_by = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='product_creator')
     title = models.CharField(max_length=255)
     artist = models.CharField(max_length=255, default='admin')
     description = models.TextField(blank=True)
@@ -26,13 +29,9 @@ class Product(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
-    
     class Meta:
         verbose_name_plural = 'Products'
         ordering = ('-created_at',)
 
     def __str__(self):
         return self.title
-    
-
-
