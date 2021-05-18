5a3e215c2f06b295ee2d2d442a17f346a3aefae8
Daniel Santos <dados.dan.santos@gmail.com>
2021-05-18 10:59:11 -0300

    feat: create delete basket item functionality

--- basket/views.py
+++ basket/views.py
@@ -1,5 +1,30 @@
-from django.shortcuts import render
+from django.http import JsonResponse, response
+from django.shortcuts import get_object_or_404, render
+
+from basket.basket import Basket
+
+from store.models import Product
 
 
 def basket_summary(request):
-    return render(request, 'store/basket/summary.html')
+    basket = Basket(request)
+    return render(request, 'store/basket/summary.html', {'basket': basket})
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'post':
+        product_id = int(request.POST.get('productid'))
+        product_quantity = int(request.POST.get('productqty'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product, quantity=product_quantity)
+        basket_quantity = basket.__len__()
+        response = JsonResponse({'qty': basket_quantity})
+        return response
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'delete':
+        product_id = int(request.POST.get('productid'))
+        basket.delete(product_id=product_id)
+        response = JsonResponse({'Success': True})
+        return response
